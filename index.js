600f687fc7fa28484beaf67120aca0c1215fa499
ryzetech <ryzetechde@gmail.com>
2021-05-11 19:37:54 +0200

    🐛 image not showing up

--- index.js
+++ index.js
@@ -1189,7 +1189,7 @@ client.on("message", async (message) => {
               .chd(plotdata)                                              // data
               .chs("999x500");                                            // image size
 
-            img.toFile("./data/graph_ban.png"); // save the image
+            await img.toFile("./data/graph_ban.png"); // save the image
 
             message.channel.send(
               new Discord.MessageEmbed()
