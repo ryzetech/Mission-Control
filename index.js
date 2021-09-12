0a4ff9f5de75deed6947bed2f4ccf805886729b2
ryzetech <mr.2702.mm@gmail.com>
2021-09-12 16:37:07 +0200

    🐛 xml2js doesn't parse in time

--- index.js
+++ index.js
@@ -1464,79 +1464,117 @@ client.on("message", async (message) => {
     message.channel.startTyping();
 
     let res = await axios.get(`https://api.wolframalpha.com/v2/query?input=${encodeURIComponent(input)}&appid=${wolfram_auth}`);
-    let data = await xml2js.parseStringPromise(res.data).queryresult;
 
     message.channel.stopTyping();
 
-    // catch errors
-    if (data.$.error === "true") {
-      return message.channel.send(
-        new Discord.MessageEmbed()
-        .setColor(embedColorFail)
-        .setAuthor("Wolfram|Alpha", embedPB)
-        .setTitle("❌ Error!")
-        .setDescription("You can't get salary at the moment!")
-        .addFields(
-          {
-            name: "Message",
-            value: `\`${data.error[0].msg}\``,
-            inline: true
-          },
-          {
-            name: "Error Code",
-            value: data.error[0].code,
-            inline: true
-          }
-        )
-        .setTimestamp()
-        .setFooter(`Requested by ${message.author.tag}`)
-      );
+    //               ..,,:::;;iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii;::1ffLLLLLLLLLLL
+    //              ..,,::;iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii;::::iiiiiiii;ii11ttttttttt
+    //           .,,,:;;;;iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii:. ,:;iiiiiiiiiiiii;;;;;;;;;;
+    //         .,..,:;iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii:. ,t1iiiiiiiiiiiiiiiiiiiiiiiii
+    //            :iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii;,  ;C00L1iiiiiiiiiiiiiiiiiiiiiii
+    //           :iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii;,  :tG0000Gfiiiiiiiiiiiiiiiiiiiiii
+    // ::::::,,..;iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii:. ,tG00000000Liiiiiiiiiiiiiiiiiiiii
+    // ;;;;iiii::iiiiiiiiiiiiiiiiiiiii;iiiiiiiiii;.  ;C00000000000C1iiiiiiiiiiiiiiiiiii
+    // 11i;::;i;;iiiiiiiiiiiiiiii;;;iiii;iiiiii;,  :f0Ct11fC0000000G1;iiiiiiii;;;;i:;ii
+    // 11111i;:;iiiiiiiiiiiiiiiiiiiiiiii:;iii;,  ,tG00i,. .,iL000000L,:iii;;;;itLCf:ii;
+    // 1111111i:;iiiiiiiiiiiiiiiiiiii;;iii;:,..,:t0000t,    .:L0000C;,,;;;ifCG88@@L:;;:
+    // 11111111i;iiiiiiiiiiiiiiiiiiiiiii;;;:::;ii;f000Gt:.. .:f000L;,::;tG8@@@@88Gt:;;i
+    // 11111111i;iiiiiiiiiiiiiiiiiiiiiii,.:,::;;ii;f0000Cft1tLG0Gf::,,iC8@@8@80CC0C;iii
+    // 11111111;;iiiiiiiiiiiiiiiiiiii;,.  :;;:;:;ii;1C00000000GLi::,iC8@88@80CG8@@81;;i
+    // 1111iii;;iiiiiiiiiiiiiiiiiii;,  ..:iiii;:;iii;itCGGGCLti;:::f8@888@8CC8@@@@@8L1i
+    // 1111;;;;;;;;iiiiiiiiiiiiii;.  .iLCt;;ii;;iiiiii;;i;:::,:::;L8@888@0L0@@@@@@@@8Gf
+    // 1111111i;;:;iiiiiiiiiiii;,  .1C0000Gt;;iiiiiiiiiii;:;;;;;;G@8888@GC8@@@@@@@8GG08
+    // 111111i;;;;:iiiiiiiiiii:.  :LGLLLCG00C1;;iiiiiiiiiiiiii;iG@888@@CC@@@@@@@0CG8@@@
+    // i1111111111;;iiiiiiiii:  .1GL;,..,iL000Li;iiiiiiiiiiii;i0@888@@0C@@@@@@8CC8@@@@@
+    // .,;i11111111;;iiiiiii:  ;L001,    .:f000Gi:iiiiiiiii;;;C@8888@@@@@@@@8GC8@@@@@@@
+    //    .,:;iiiii;:;iiiii:  iGG00L:.    .;G000L,;iiii;::::::C8@@8@@@@@@@@0C0@@@@@@@@@
+    //         ..... .:iiii: .tGGG00L1;,..,iGGGGf.:;iii;,..,. .:f88@@@@@@@CC8@@@@@@@@@@
+    //                 :iii;.:;tGGGG00GCLffCGGGL:,:iiii;..f0i    L@@@@@@@@8@@@@@88@@@@@
+    //                  ,:ii;ii;1LGGGGG00GGGGGf;::;ii;;, ;8t.    f@@@@@@@@@@@@@@@8@@@@@
+    //                    ,;iiiii;1LGGGGGGGGLi,:;:;i1tfi .:      C@@@@@@@@@8GLC8@@@@@@@
+    //                     .:iiiii;i1fLCCCCt:,:;;:1C08@0i:::::;;i0@@@@@@@0LiiG@@@@@@@@@
+    //                       ,;iiiiii;iii;:,:;;;;t8@@8@@@88888@@@@@8088G1::f8@@@@@@@@@@
+    //                        .:iiiiiiii;;;;iii;f8@@@@@@@@@@@@@@@@@@8Gi,,t08G0@@@@@@@@@
+    //                          ,;iiiiiiiiiiii;i8@80GG@@@@@@@@@@@@@@8t:1G80G88888@@@@@@
+    //                           .:iiiiiiiiiii;180CC0@@@@@@@@@@@@@@@CL08008@@888@@@@@@@
+    //                             :iiiiiiiiii;;CG88@@@@@8GG0@@@@@@@@@@@@@888@@@@@@@@@@
+    //                             ,iiiiiiii;:::0@@8@@8GGG0@@@@@@888@@@@@@@@@@@@@@@@@@@
 
-    }
+    // "fuck bro this hack is stupid af"
 
-    // loop through assumptions (if defined)
-    if (data.assumptions !== undefined) {
-      let embedFields = [];
+    xml2js.parseString(res.data).then(function (data) {
+      data = data.queryresult;
 
-      // create new field for every assumption
-      for (let ass of data.assumptions) {
-        embedFields.push(new EzField(ass.$.type, `${ass.$.word}\n=> ${ass.value[0].$.desc}`))
+      // catch errors
+      if (data.$.error === "true") {
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setColor(embedColorFail)
+            .setAuthor("Wolfram|Alpha", embedPB)
+            .setTitle("❌ Error!")
+            .setDescription("You can't get salary at the moment!")
+            .addFields(
+              {
+                name: "Message",
+                value: `\`${data.error[0].msg}\``,
+                inline: true
+              },
+              {
+                name: "Error Code",
+                value: data.error[0].code,
+                inline: true
+              }
+            )
+            .setTimestamp()
+            .setFooter(`Requested by ${message.author.tag}`)
+        );
+
       }
 
-      await message.channel.send(
-        new Discord.MessageEmbed()
-        .setColor("#fce63c")
-        .setAuthor("Wolfram|Alpha", embedPB)
-        .setTitle("Assumptions")
-        .addFields(embedFields)
-        .setTimestamp()
-        .setFooter(`Requested by ${message.author.tag}`)
-      );
-    }
+      // loop through assumptions (if defined)
+      if (data.assumptions !== undefined) {
+        let embedFields = [];
 
-    // loop through pods (maximum defined by config)
-    let podcount = parseInt(data.$.numpods) <= wolfram_maxpods ? parseInt(data.$.numpods) : wolfram_maxpods;
+        // create new field for every assumption
+        for (let ass of data.assumptions) {
+          embedFields.push(new EzField(ass.$.type, `${ass.$.word}\n=> ${ass.value[0].$.desc}`))
+        }
 
-    for (let i = 0; i < podcount; i++) {
-      let pod = data.pod[i];
-      let embedFields = [];
-
-      // create new field for every subpod
-      for (let subpod of pod.subpod) {
-        embedFields.push(new EzField(subpod.$.title, subpod.plaintext[0]));
+        await message.channel.send(
+          new Discord.MessageEmbed()
+            .setColor("#fce63c")
+            .setAuthor("Wolfram|Alpha", embedPB)
+            .setTitle("Assumptions")
+            .addFields(embedFields)
+            .setTimestamp()
+            .setFooter(`Requested by ${message.author.tag}`)
+        );
       }
 
-      // create new message for every pod
-      await message.channel.send(
-        new Discord.MessageEmbed()
-          .setColor(embedColorStandard)
-          .setAuthor("Wolfram|Alpha", embedPB)
-          .setTitle(pod.$.title)
-          .addFields(embedFields)
-          .setTimestamp()
-          .setFooter(`Requested by ${message.author.tag} | Pod ${i}/${podcount}`)
-      );
-    }
+      // loop through pods (maximum defined by config)
+      let podcount = parseInt(data.$.numpods) <= wolfram_maxpods ? parseInt(data.$.numpods) : wolfram_maxpods;
+
+      for (let i = 0; i < podcount; i++) {
+        let pod = data.pod[i];
+        let embedFields = [];
+
+        // create new field for every subpod
+        for (let subpod of pod.subpod) {
+          embedFields.push(new EzField(subpod.$.title, subpod.plaintext[0]));
+        }
+
+        // create new message for every pod
+        await message.channel.send(
+          new Discord.MessageEmbed()
+            .setColor(embedColorStandard)
+            .setAuthor("Wolfram|Alpha", embedPB)
+            .setTitle(pod.$.title)
+            .addFields(embedFields)
+            .setTimestamp()
+            .setFooter(`Requested by ${message.author.tag} | Pod ${i}/${podcount}`)
+        );
+      }
+    });
   }
 
   //// ECONOMY SECTION
