c5bcbeb25b96730d9d80d58a273d96395b60fdd4
ryzetech <ryzetechde@gmail.com>
2021-10-30 10:27:17 +0200

    ⏪ Revert 2fe1d8f

--- index.js
+++ index.js
@@ -39,7 +39,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({
   intents: [
-    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MEMBERS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
   ],
 });
 const si = require("systeminformation");
